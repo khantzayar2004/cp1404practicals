77166dc0f22fdd5cfb9384a90204be4d94e5c933
Khant Zay Yar <khantzayyar@my.jcu.edu.au>
2024-03-15 15:12:28 +0800

    Hex colours exercise

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,19 @@
+"""
+CP1404 Practical 05
+Hexadecimal colour lookup
+"""
+
+COLOUR_CODES = {"AliceBlue": "#f0f8ff", "AntiqueWhite": "#faebd7",
+                "AntiqueWhite1": "#ffefdb", "AntiqueWhite2": "#eedfcc",
+                "AntiqueWhite3": "#cdc0b0", "AntiqueWhite4": "#8b8378",
+                "aquamarine1": "#7fffd4", "aquamarine2": "#76eec6",
+                "aquamarine4": "#458b74", "azure1": "#f0ffff",
+                "azure2": "#e0eeee", "azure3": "#c1cdcd", "azure4": "#838b8b",
+                "beige": "#f5f5dc", "bisque1": "#ffe4c4"}
+
+colour_name = input("Enter a colour name: ")
+while colour_name != "":
+    # Using the get dictionary method
+    # It means you will get none if the key is not found
+    print(f"The code for \"{colour_name}\" is {COLOUR_CODES.get(colour_name)}")
+    colour_name = input("Enter a colour name: ")
