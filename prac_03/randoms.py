91b9103081cf00e47afe856b2161c86641671213
Khant Zay Yar <khantzayyar@my.jcu.edu.au>
2024-03-15 14:50:55 +0800

    Random Numbers exercise

---
+++ prac_03/randoms.py
@@ -0,0 +1,29 @@
+"""
+CP1404 - Practical 3 - Randoms
+Student Name - Khant Zay Yar
+Student ID   - 14052564
+"""
+
+# What did you see on line 1?
+# What was the smallest number you could have seen, what was the largest?
+
+# The smallest number was 6 and the largest number was 11.
+
+# What did you see on line 2?
+# What was the smallest number you could have seen, what was the largest?
+# Could line 2 have produced a 4?
+
+# Number 7 is shown on line 2
+# The smallest number was 3 and the largest number was 9.
+# No
+
+# What did you see on line 3?
+# What was the smallest number you could have seen, what was the largest?
+
+# 4.730333025653329 is shown.
+# The smallest number was 3.5 and the largest number was 5.3.
+
+# Write code, not a comment, to produce a random number between 1 and 100 inclusive.
+
+import random
+print(random.randint(1, 101))
