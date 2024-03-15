4bada40f1c5756956f3f2c17bc1215bd11564df8
Khant Zay Yar <khantzayyar@my.jcu.edu.au>
2024-03-15 15:00:52 +0800

    Income exercise

---
+++ prac_04/total_income.py
@@ -0,0 +1,27 @@
+"""
+CP1404 - Prac 04
+"""
+
+
+def main():
+    """Display income report for incomes"""
+    incomes = []
+    number_of_months = int(input("How many months? "))
+
+    for month in range(1, number_of_months + 1):
+        income = float(input(f"Enter income for month {month}: "))
+        incomes.append(income)
+
+    print_report(incomes)
+
+
+def print_report(incomes):
+    """Print a report based on incomes"""
+    print("\nIncome Report\n-------------")
+    total = 0
+    for month, income in enumerate(incomes, 1):
+        total += income
+        print(f"Month {month:2} - Income: ${income:10.2f} Total: ${total:10.2f}")
+
+
+main()
