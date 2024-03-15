8d78429887c419597645d33ca4ee7a779f11bf3e
Khant Zay Yar <khantzayyar@my.jcu.edu.au>
2024-03-15 15:13:34 +0800

    An email program

---
+++ prac_05/emails.py
@@ -0,0 +1,31 @@
+"""
+CP1404 Practical 05
+Emails
+"""
+
+
+def main():
+    """Store the user's email and name in a dictionary."""
+    email_to_name = {}
+    email = input("Email: ")
+    while email != "":
+        username = get_name(email)
+        confirmation = input(f"Is your name {username}? (Y/N) ")
+        if confirmation.upper() != "Y" and confirmation != "":
+            username = input("Name: ")
+        email_to_name[email] = username
+        email = input("Email: ")
+
+    for email, username in email_to_name.items():
+        print(username, f"({email})")
+
+
+def get_name(email):
+    """Get name from email."""
+    prefix = email.split("@")[0]
+    parts = prefix.split(".")
+    username = " ".join(parts).title()
+    return username
+
+
+main()
